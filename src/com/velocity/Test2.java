6c2451c988db4573d5f6d12155f49f0e71511a24
Prafulla Shirode <shirodeprafulla@gmail.com>
2023-01-08 17:23:37 +0530

    second commit

---
+++ src/com/velocity/Test2.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test2 {
+	public static void main(String[] args) {
+		Set<Integer> set=new HashSet<Integer>();
+		set.add(10);
+		set.add(20);
+		set.add(30);
+		for(Integer a:set) {
+			System.out.println(a);
+		}
+	}
+
+}
