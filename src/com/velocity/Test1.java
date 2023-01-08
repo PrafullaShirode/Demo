c917083ed129c6404d08fdd74a2a1ace53fdd570
Prafulla Shirode <shirodeprafulla@gmail.com>
2023-01-08 17:20:30 +0530

    first commit

---
+++ src/com/velocity/Test1.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("amay");
+		list.add("vikas");
+		list.add("mahesh");
+		System.out.println(list);
+	}
+
+}
